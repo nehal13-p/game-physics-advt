6027fc9359f3c5fd4c2eb422e3ec520127974ea6
nehal13-p <82352013+nehal13-p@users.noreply.github.com>
2021-12-23 15:41:21 +0530

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float degreesPersecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPersecond, 0f), Space.World);
+
+    }
+}
